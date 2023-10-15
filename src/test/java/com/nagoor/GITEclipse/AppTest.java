a634537f065abdf0e21314f7246a331837f8f560
nagoorbabu2596 <nagoor2596@gmail.com>
2023-10-15 13:12:10 +0530

    new project

---
+++ src/test/java/com/nagoor/GITEclipse/AppTest.java
@@ -0,0 +1,20 @@
+package com.nagoor.GITEclipse;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class AppTest {
+	@Test
+	public void shouldAnswerWithTrue() {
+		assertTrue(true);
+	}
+	@Test
+	public void should1() {
+		assertTrue(true);
+	}
+	@Test
+	public void should2() {
+		assertTrue(true);
+	}
+}
